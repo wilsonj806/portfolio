8ef968baaa1b3b40fd68e2de3470f749f8f35229
Wilson <wilsonj806@gmail.com>
2020-02-19 12:26:43 -0500

    Add some minimal stuff
    Add the below pages:
    - About/ Blog/ Projects
    
    Update main.js with global styles from the new main.css file
    
    Add a Blog/ Project template file
    
    Add a bare minimum blog post in blog/post-1.md

---
+++ src/templates/Blog.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- Figure out how to make this a component instead -->
+    <VueRemarkContent/>
+  </div>
+</template>
+
+<page-query>
+query Blog($id: ID!) {
+  blog(id:$id) {
+    id
+    path
+    content
+  }
+}
+</page-query>
+
+<style scoped>
+.blog {
+  margin: auto;
+  max-width: 65%;
+}
+</style>
---
+++ src/templates/Project.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- Figure out how to make this a component instead -->
+    <div class="project" v-html="$page.project.content"/>
+  </div>
+</template>
+
+<page-query>
+query Project($id: ID!) {
+  Project(id:$id) {
+    id
+    path
+    content
+  }
+}
+</page-query>
+
+<style scoped>
+.blog {
+  margin: auto;
+  max-width: 65%;
+}
+</style>
