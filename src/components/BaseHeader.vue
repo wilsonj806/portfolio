610186534aaec24c6b1ce0e897b20cf267c8e3a9
Wilson <wilsonj806@gmail.com>
2020-02-19 15:53:20 -0500

    Add Nav components for Desktop and Mobile
    Minimal styling for components

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <NavDesktop v-if="isMobile === false"/>
+    <NavMobile v-else/>
+  </header>
+</template>
+
+<script>
+import NavDesktop from './NavDesktop.vue'
+import NavMobile from './NavMobile.vue'
+export default {
+  components: {
+    NavDesktop,
+    NavMobile
+  },
+  data() {
+    return {
+      isMobile: window.innerWidth <= 768 || screen.width <= 768
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/NavDesktop.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <div>
+      <g-link class="name">Wilson Jiang</g-link>
+    </div>
+    <div class="primary-nav-links">
+      <g-link to="/about">About</g-link>
+      <g-link to="/projects">Projects</g-link>
+      <g-link to="/blog">Blog</g-link>
+      <g-link to="/contact">Contact</g-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavDesktop'
+}
+</script>
+
+<style scoped>
+nav {
+  width: 100%;
+  display: flex;
+  margin: 1rem;
+  padding: 0.25rem 0;
+  justify-content: space-between;
+  align-items: baseline;
+}
+
+.primary-nav-links > a {
+  margin: 0 0.5rem;
+}
+
+a {
+  font-size: 1.75rem;
+}
+</style>
