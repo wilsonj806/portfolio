14b62bcca25f799b5131a7be4f8a7c65e425067e
Wilson <wilsonj806@gmail.com>
2020-02-23 15:32:38 -0500

    Add basic 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,29 @@
+<template>
+<section>
+  <h1>404 Page not found!</h1>
+  <h2>Perhaps you meant:</h2>
+  <ul>
+    <li v-for="(page, index) in $page.allPage" :key="index">
+      <g-link :to="page.path">{{ page.path }}</g-link>
+    </li>
+  </ul>
+</section>
+</template>
+
+<page-query>
+query {
+  allPage {
+    path
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: '404'
+}
+</script>
+
+<style>
+
+</style>
