4bd304e54611d2055ee0ddd49ba5bf9da22514e8
Wilson <wilsonj806@gmail.com>
2020-06-10 16:47:08 -0400

    Add BaseNav and update styling

---
+++ src/components/BaseNav.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav>
+    <div class='nav-header'>
+      <span>WJ</span>
+    </div>
+    <div class="nav-links">
+      <ul class='nav-list'>
+        <!-- <li><g-link to='/works'>Works</g-link></li> -->
+        <!-- <li><g-link to='/blog'>Blog</g-link></li> -->
+        <li class='nav-list-item'>
+          <a class='nav-link' href='https://www.dropbox.com/s/soxnd60u5du2x66/Wilson-Jiang-Resume.pdf?dl=07&raw=1'>
+            Resume
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style src='../styles/nav.css' scoped>
+</style>
