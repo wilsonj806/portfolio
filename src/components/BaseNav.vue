320c2a2790f3c9af035bfe9cedbc93bfaa7e013b
Wilson <wilsonj806@gmail.com>
2020-06-11 13:09:46 -0400

    Add more styling to Nav

--- src/components/BaseNav.vue
+++ src/components/BaseNav.vue
@@ -8,9 +8,7 @@
         <!-- <li><g-link to='/works'>Works</g-link></li> -->
         <!-- <li><g-link to='/blog'>Blog</g-link></li> -->
         <li class='nav-list-item'>
-          <a class='nav-link' href='https://www.dropbox.com/s/soxnd60u5du2x66/Wilson-Jiang-Resume.pdf?dl=07&raw=1'>
-            Resume
-          </a>
+          <a class='nav-link' href='https://www.dropbox.com/s/soxnd60u5du2x66/Wilson-Jiang-Resume.pdf?dl=07&raw=1'>Resume</a>
         </li>
       </ul>
     </div>
