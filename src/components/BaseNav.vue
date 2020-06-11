6810f3d5318d96ee9da35f1d3c9801b02d910376
Wilson <wilsonj806@gmail.com>
2020-06-11 16:02:13 -0400

    Small update to BaseNav.vue

--- src/components/BaseNav.vue
+++ src/components/BaseNav.vue
@@ -8,6 +8,7 @@
         <!-- <li><g-link to='/works'>Works</g-link></li> -->
         <!-- <li><g-link to='/blog'>Blog</g-link></li> -->
         <li class='nav-list-item'>
+          <!-- Fix the darkness on the hover state -->
           <a class='nav-link' href='https://www.dropbox.com/s/soxnd60u5du2x66/Wilson-Jiang-Resume.pdf?dl=07&raw=1'>Resume</a>
         </li>
       </ul>
@@ -17,7 +18,7 @@
 
 <script>
 export default {
-
+  name: 'BaseNav'
 }
 </script>
 
