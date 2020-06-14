8c84269c591c273b81a8275d2e36494205b54a94
Wilson <wilsonj806@gmail.com>
2020-06-14 13:40:17 -0400

    Add more styling to Blog template

--- src/templates/Blog.vue
+++ src/templates/Blog.vue
@@ -1,7 +1,9 @@
 <template>
   <Layout>
     <!-- Figure out how to make this a component instead -->
-    <VueRemarkContent/>
+    <article class='blog'>
+      <VueRemarkContent/>
+    </article>
   </Layout>
 </template>
 
@@ -18,6 +20,29 @@ query Blog($id: ID!) {
 <style scoped>
 .blog {
   margin: auto;
-  max-width: 65%;
+  width: 75%;
+}
+
+.blog >>> p > code {
+  background: rgba(144, 238, 144, 0.5);
+}
+
+.blog >>> pre {
+  background: rgba(144, 238, 144, 0.5);
+  padding: 24px;
+  white-space: normal;
+  width: 80%;
+  margin: auto;
+  color: rgb(26, 26, 26);
+}
+
+.blog >>> a:link,.blog >>> a:visited {
+  text-decoration: none;
+  color: rgb(190, 105, 0);
+}
+
+.blog >>> a:hover {
+  text-decoration: underline;
+  color: darkorange;
 }
 </style>
